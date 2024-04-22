cebb95abe27184cdc2cb81f595613abc7841dbb6
Connor Maclachlan <connormac000@gmail.com>
2024-04-22 13:40:00 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,16 @@ import React from 'react';
 import styled from 'styled-components';
 import Header from './components/Header';
 import NewDrop from './components/NewDrop';
-import Gallery from './components/Gallery';
 
 const StyledBody = styled.div`
 `;
 
 function App() {
-  const imageUrls = [
-    {'image': './assets/dropgalllery/andy1.png'},
-    {'image': './assets/dropgalllery/andy2.png'},
-    {'image': './assets/dropgalllery/ronan3.png'},
-    {'image': './assets/dropgalllery/ronan4.png'},
-  ];
+
   return (
     <StyledBody>
       <Header />
       <NewDrop />
-      <Gallery images={imageUrls}/>
     </StyledBody>
   )
 }
