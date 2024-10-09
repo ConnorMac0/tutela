0493d97b9f73c8d3b61fa07cc42a8138988fa117
ConnorMac0 <connormac000@gmail.com>
2024-10-09 13:39:49 -0700

    Update: Finished frontend list products API functionality

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,7 +1,7 @@
-import React, { createContext, useState } from "react";
-import { products } from '../assets/assets'
+import React, { createContext, useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
+import axios from 'axios';
 
 export const ShopContext = createContext();
 
@@ -9,8 +9,10 @@ const ShopContextProvider = (props) => {
 
     const currency = "$";
     const delivery_fee = 10;
+    const backendURL = import.meta.env.VITE_BACKEND_URL;
     const [cartItems, setCartItems] = useState({});
     const navigate = useNavigate();
+    const [products, setProducts] = useState([]);
 
     const addToCart = async (itemId,size) => {
         let cartData = structuredClone(cartItems);
@@ -74,10 +76,33 @@ const ShopContextProvider = (props) => {
         return cartTotal;
     }
 
+    const getProductData = async () => {
+        try {
+            const response = await axios.get(backendURL + '/api/product/list')
+            if (response.data.success) {
+                setProducts(response.data.products)
+            } else {
+                toast.error(response.data.message, {
+                    position: "top-center"
+                  });
+            }
+            
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message, {
+                position: "top-center"
+              });
+        }
+    }
+
+    useEffect(()=>{
+        getProductData();
+    },[])
+
     const value = {
         products, currency, delivery_fee,cartItems, 
         addToCart, getCartCount, removeItem,
-        getCartTotal, navigate
+        getCartTotal, navigate, backendURL
     }
 
     return (
